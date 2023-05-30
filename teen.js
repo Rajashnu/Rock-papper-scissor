984afa339d50e0a519d343b0da6b0d3c010687f2
Rajashnu <rajashnushakya@gmail.com>
2023-05-30 22:46:45 +0545

    ifelse

---
+++ teen.js
@@ -0,0 +1,19 @@
+let a;
+a = prompt("enter your age:");
+let b;
+b = prompt("enter your gender (f) female (m) male:");
+if (a<=18){
+if (b==m){
+    console.log("teenage boy");
+}
+}
+else if {
+    if (a < = 18){
+        if (b==f){
+            console.log("teenage girl")
+        }
+    }
+}
+else{
+    console.log("not a teenage")
+}
